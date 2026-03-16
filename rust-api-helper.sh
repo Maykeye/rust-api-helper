b041a3075a062a7a5eb037e3dad9da2541dfc809
Maykeye <maykeye@gmail.com>
2026-03-16 21:18:05 +0600

    .sh:fix to print all files from subfolders
    Didn't include shopt in original script -- pain

--- rust-api-helper.sh
+++ rust-api-helper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+shopt -s globstar
+
 if [ z"$1" == z"" ]; then
     echo "CALL $0 <dir>"
     exit 1
@@ -6,7 +8,7 @@ fi
 
 cd $1
 
-for p in */**.rs; do
+for p in **/*.rs; do
     echo "FILE:" "$p"
     rust-api-helper "$p"
 done
